0a45926aa27c9259171589234e043bb1463395ad
robertli <robertg.li@yahoo.com>
2023-04-17 21:43:12 -0400

    finishing yelp app

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+    @ratings = []
+    5.times do |i|
+      @ratings << ['⭐️' * (i + 1), (i + 1)]
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
